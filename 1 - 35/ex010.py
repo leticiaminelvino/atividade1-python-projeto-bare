08b9f5bd41db7a4ef4ce24ec4a3c25a09564cd42
Adonaay <161964280+Adonaay@users.noreply.github.com>
2024-03-18 17:29:40 -0400

    exercicio10.py

--- 1 - 35/ex010.py
+++ 1 - 35/ex010.py
@@ -1,9 +1,11 @@
-"""Autores: ADONAY SOUZA FERREIRA
-            ALINE DAFFINY FERREIRA GOMES
-            LETÍCIA MINELVINO DA_COSTA
 """
+Alunos: 
+ADONAY SOUZA FERREIRA
+ALINE DAFFINY FERREIRA GOMES
+LETÍCIA MINELVINO DA COSTA
 
-# Faça um programa que leia quantos reais uma pessoa tem na carteira e quantos dólares ela pode comprar.
+Exercicio 10:
+Faça um programa que leia quantos reais uma pessoa tem na carteira e quantos dólares ela pode comprar."""
 
 troca = float(input('Quanto dinheiro você tem na cateira? R$ '))
 print('Com R${:.2f} você pode comprar U${:.2f}'.format(troca, troca/4.96))
