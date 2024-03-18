0644e3b0b6e61e39a421ecd4333258c21afc3772
Adonaay <161964280+Adonaay@users.noreply.github.com>
2024-03-18 17:44:48 -0400

    Update ex033.py

--- 1 - 35/ex033.py
+++ 1 - 35/ex033.py
@@ -1,9 +1,11 @@
-"""Autores: ADONAY SOUZA FERREIRA
-            ALINE DAFFINY FERREIRA GOMES
-            LETÍCIA MINELVINO DA_COSTA
 """
+Alunos: 
+ADONAY SOUZA FERREIRA
+ALINE DAFFINY FERREIRA GOMES
+LETÍCIA MINELVINO DA COSTA
 
-# Faça um programa que leia 3 números e mostre qual deles é o maior e qual o menor:
+Exercicio 33:
+Faça um programa que leia 3 números e mostre qual deles é o maior e qual o menor: """
 
 n1 = int(input('Digite o primeiro valor: '))
 n2 = int(input('Digite o segundo valor: '))
