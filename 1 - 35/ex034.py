7c85c22e9bc7c4228d45c5087b2a46368195dd8a
Adonaay <161964280+Adonaay@users.noreply.github.com>
2024-03-18 17:45:21 -0400

    exercicio34.py

--- 1 - 35/ex034.py
+++ 1 - 35/ex034.py
@@ -1,11 +1,13 @@
-"""Autores: ADONAY SOUZA FERREIRA
-            ALINE DAFFINY FERREIRA GOMES
-            LETÍCIA MINELVINO DA_COSTA
 """
+Alunos: 
+ADONAY SOUZA FERREIRA
+ALINE DAFFINY FERREIRA GOMES
+LETÍCIA MINELVINO DA COSTA
 
-# Escreva um programa que pergunte o salário de um funcionário e calcule o valor do aumento.
-# Para salários superiores a R$1250,00 calcule um aumento de 10%.
-# Para os inferiores ou iguais, o aumento é de 15%.
+Exercicio 34:
+Escreva um programa que pergunte o salário de um funcionário e calcule o valor do aumento.
+Para salários superiores a R$1250,00 calcule um aumento de 10%.
+Para os inferiores ou iguais, o aumento é de 15%. """
 
 salario = float(input('Digite o salário atual do funcionário: '))
 novo_salario = 0
