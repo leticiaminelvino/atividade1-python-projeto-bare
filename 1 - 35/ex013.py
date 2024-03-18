f940f3a4cbc0648ba6435d223c75bfbc7980a7ca
Adonaay <161964280+Adonaay@users.noreply.github.com>
2024-03-18 17:32:15 -0400

    exercicio13.py

--- 1 - 35/ex013.py
+++ 1 - 35/ex013.py
@@ -1,10 +1,12 @@
-"""Autores: ADONAY SOUZA FERREIRA
-            ALINE DAFFINY FERREIRA GOMES
-            LETÍCIA MINELVINO DA_COSTA
 """
+Alunos: 
+ADONAY SOUZA FERREIRA
+ALINE DAFFINY FERREIRA GOMES
+LETÍCIA MINELVINO DA COSTA
 
-# Faça um programa que leia o salário do funcionário e informe o salario antes e apos o reajuste com
-# adicional de 15%
+Exercicio 13:
+Faça um programa que leia o salário do funcionário e informe o salario antes e apos o reajuste com
+adicional de 15%. """
 
 salario = float(input('Infome o salário do funcionário: R$ '))
 print('O funcionário que ganhava R$ {:.2f}, com reajuste de 15%, receberá R${:.2f}'
