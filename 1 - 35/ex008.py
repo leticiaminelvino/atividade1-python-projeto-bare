bf9bbef1802eb3218b10cf6fbbb3f95825f5ad8b
Adonaay <161964280+Adonaay@users.noreply.github.com>
2024-03-18 17:28:25 -0400

    exercicio08.py

--- 1 - 35/ex008.py
+++ 1 - 35/ex008.py
@@ -1,10 +1,12 @@
-"""Autores: ADONAY SOUZA FERREIRA
-            ALINE DAFFINY FERREIRA GOMES
-            LETÍCIA MINELVINO DA_COSTA
 """
+Alunos: 
+ADONAY SOUZA FERREIRA
+ALINE DAFFINY FERREIRA GOMES
+LETÍCIA MINELVINO DA COSTA
 
-# Faça um programa para inserir a distância em metros e que forneça o valor correspondente em
-# km, hm, dam, dm, cm , mm
+Exercicio 08:
+Faça um programa para inserir a distância em metros e que forneça o valor correspondente em
+km, hm, dam, dm, cm , mm. """
 
 d = float(input('Insira o valor da distancia em metros: ',))
 print('A medida de {} metros corresponde a: \n {} km \n {}hm \n {}dam \n {}dm \n {}cm \n {}mm'
