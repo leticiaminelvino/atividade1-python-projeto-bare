39a640066cfd00626d553f5d57ac16989aac9c00
Adonaay <161964280+Adonaay@users.noreply.github.com>
2024-03-18 17:39:21 -0400

    exercicio24.py

--- 1 - 35/ex024.py
+++ 1 - 35/ex024.py
@@ -1,10 +1,12 @@
-"""Autores: ADONAY SOUZA FERREIRA
-            ALINE DAFFINY FERREIRA GOMES
-            LETÍCIA MINELVINO DA_COSTA
 """
+Alunos: 
+ADONAY SOUZA FERREIRA
+ALINE DAFFINY FERREIRA GOMES
+LETÍCIA MINELVINO DA COSTA
 
-# Faça um programa que leia a cidade de nascimento ignorando os espaços e se ela começar com
-# 'Santo' indicar 'True'
+Exercicio 24:
+Faça um programa que leia a cidade de nascimento ignorando os espaços e se ela começar com
+'Santo' indicar 'True'. """
 
 cidade = str(input('Em que cidade você nasceu: ')).strip()
 print(cidade.upper()[:5] == 'SANTO')
