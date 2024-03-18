2cc361e005298bd39c04f24f8b5df6bc1ec8d5a5
Adonaay <161964280+Adonaay@users.noreply.github.com>
2024-03-18 17:31:21 -0400

    exercicio12.py

--- 1 - 35/ex012.py
+++ 1 - 35/ex012.py
@@ -1,8 +1,11 @@
-"""Autores: ADONAY SOUZA FERREIRA
-            ALINE DAFFINY FERREIRA GOMES
-            LETÍCIA MINELVINO DA_COSTA
 """
-# Faça um programa que peça o valor do produto e mostre na tela o valor do produto antes e depois do desconto de 5%.
+Alunos: 
+ADONAY SOUZA FERREIRA
+ALINE DAFFINY FERREIRA GOMES
+LETÍCIA MINELVINO DA COSTA
+
+Exercicio 12:
+Faça um programa que peça o valor do produto e mostre na tela o valor do produto antes e depois do desconto de 5%."""
 
 preco = float(input('Insira o valor do produto: R$ '))
 print('O produto que custava R${:.2f}, na promoção com desconto de 5% vai passar a custar R${:.2f}.'
