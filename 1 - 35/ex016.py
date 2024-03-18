55f19b6f466a80e7c4b725729525fbededfa89c5
Adonaay <161964280+Adonaay@users.noreply.github.com>
2024-03-18 17:34:25 -0400

    exercicio16.py

--- 1 - 35/ex016.py
+++ 1 - 35/ex016.py
@@ -1,9 +1,11 @@
-"""Autores: ADONAY SOUZA FERREIRA
-            ALINE DAFFINY FERREIRA GOMES
-            LETÍCIA MINELVINO DA_COSTA
 """
+Alunos: 
+ADONAY SOUZA FERREIRA
+ALINE DAFFINY FERREIRA GOMES
+LETÍCIA MINELVINO DA COSTA
 
-# Faça um programa que peça um número real qualquer e informe o número digitado e a sua parte inteira.
+Exercicio 16:
+Faça um programa que peça um número real qualquer e informe o número digitado e a sua parte inteira. """
 
 from math import floor
 num = float(input('Digite um um número real: '))
