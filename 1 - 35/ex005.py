95e95e8370d800970b35a7d27508c932068d47ea
Adonaay <161964280+Adonaay@users.noreply.github.com>
2024-03-18 17:25:59 -0400

    Update ex005.py

--- 1 - 35/ex005.py
+++ 1 - 35/ex005.py
@@ -1,9 +1,11 @@
-"""Autores: ADONAY SOUZA FERREIRA
-            ALINE DAFFINY FERREIRA GOMES
-            LETÍCIA MINELVINO DA_COSTA
 """
+Alunos: 
+ADONAY SOUZA FERREIRA
+ALINE DAFFINY FERREIRA GOMES
+LETÍCIA MINELVINO DA COSTA
 
-# Faça um programa que peça um número e indique qual o valor digitado e qual o antecessor e o sucessor.
+Exercicio 05:
+Faça um programa que peça um número e indique qual o valor digitado e qual o antecessor e o sucessor."""
 
 num = int(input('Digite um número: '))
 print('O número digitado foi {}, o seu antecessor é {}, o seu sucessor é {}'.format(num, (num-1), (num+1)))
