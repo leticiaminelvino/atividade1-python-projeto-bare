4a5a514e1d54a7644e90a846eafe13542af2ccb2
Adonaay <161964280+Adonaay@users.noreply.github.com>
2024-03-18 17:40:03 -0400

    exercicio25.py

--- 1 - 35/ex025.py
+++ 1 - 35/ex025.py
@@ -1,9 +1,11 @@
-"""Autores: ADONAY SOUZA FERREIRA
-            ALINE DAFFINY FERREIRA GOMES
-            LETÍCIA MINELVINO DA_COSTA
 """
+Alunos: 
+ADONAY SOUZA FERREIRA
+ALINE DAFFINY FERREIRA GOMES
+LETÍCIA MINELVINO DA COSTA
 
-# Faça um programa que leia o nome completo de uma pessoa e informe se tem o nome Silva nele.
+Exercicio 25:
+Faça um programa que leia o nome completo de uma pessoa e informe se tem o nome Silva nele. """
 
 nome = str(input('Qual seu nome completo? '))
 print('Seu nome tem Silva? {}'.format('SILVA' in nome.upper()))
