e1337cf5022b81d37f333d43b2497c9b7665e755
Adonaay <161964280+Adonaay@users.noreply.github.com>
2024-03-18 17:41:55 -0400

    exercicio28.py

--- 1 - 35/ex028.py
+++ 1 - 35/ex028.py
@@ -1,9 +1,12 @@
-"""Autores: ADONAY SOUZA FERREIRA
-            ALINE DAFFINY FERREIRA GOMES
-            LETÍCIA MINELVINO DA_COSTA
 """
-# Escreva um programa que faça o computador 'pensar' em um número inteiro entre 0 e 5 e peça para o usuário tentar
-# descobrir qual foi o número escolhido pelo computador. O programa deverá informar se o usuário venceu ou perdeu.
+Alunos: 
+ADONAY SOUZA FERREIRA
+ALINE DAFFINY FERREIRA GOMES
+LETÍCIA MINELVINO DA COSTA
+
+Exercicio 28:
+Escreva um programa que faça o computador 'pensar' em um número inteiro entre 0 e 5 e peça para o usuário tentar
+descobrir qual foi o número escolhido pelo computador. O programa deverá informar se o usuário venceu ou perdeu. """
 
 import random
 num = int(input('Digite um número entre 0 e 5: '))
