f46d2f78778983a8f292820b49301f4336f3a60a
Adonaay <161964280+Adonaay@users.noreply.github.com>
2024-03-18 14:53:29 -0400

    Update ex035.py

--- 1 - 35/ex035.py
+++ 1 - 35/ex035.py
@@ -4,9 +4,9 @@
 """
 
 # Desenvolva um programa que leia o comprimento de 3 retas e diga ao usuário se elas podem ou não formar um triângulo.
-lado1 = float(input('Insira o tamanho da primeiro lado: '))
-lado2 = float(input('Insira o tamanhodo segundo lado: '))
-lado3 = float(input('Insira o tamanho do terceiro lado: '))
+r1 = float(input('Insira o tamanho da primeira reta: '))
+r2 = float(input('Insira o tamanho da segunda reta: '))
+r3 = float(input('Insira o tamanho da terceira reta: '))
 
 if r1 + r2 > r3 and r1 +r3 > r2 and r2 +r3> r1:
    print("Os lados informados acima formam um triângulo")
