cb915fd739ca01db5c6f8eb76fccf1bebf0a8343
Adonaay <161964280+Adonaay@users.noreply.github.com>
2024-03-18 17:33:04 -0400

    exercicio14.py

--- 1 - 35/ex014.py
+++ 1 - 35/ex014.py
@@ -1,9 +1,11 @@
-"""Autores: ADONAY SOUZA FERREIRA
-            ALINE DAFFINY FERREIRA GOMES
-            LETÍCIA MINELVINO DA_COSTA
 """
+Alunos: 
+ADONAY SOUZA FERREIRA
+ALINE DAFFINY FERREIRA GOMES
+LETÍCIA MINELVINO DA COSTA
 
-# Faça um programa que peça o valor da temperatura em °C e converta em °F.
+Exercicio 14:
+Faça um programa que peça o valor da temperatura em °C e converta em °F. """
 
 celsius = float(input('Infome a temperatura em graus Celsius: '))
 print('A temperatura de {:.2f}°C corresponde a {:.2f}°F'.format(celsius, (celsius*1.8)+32))
