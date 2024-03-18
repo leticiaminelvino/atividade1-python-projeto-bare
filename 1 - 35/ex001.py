48963b18f10313ebe5a9f00aa2e6a376b2142a8f
Adonaay <161964280+Adonaay@users.noreply.github.com>
2024-03-18 17:19:23 -0400

    exercicio01.py

--- 1 - 35/ex001.py
+++ 1 - 35/ex001.py
@@ -1,8 +1,11 @@
-"""Autores: ADONAY SOUZA FERREIRA
-            ALINE DAFFINY FERREIRA GOMES
-            LETÍCIA MINELVINO DA COSTA
 """
-# Crie um programa que escreva "Olá, mundo!" na tela.
+Alunos:
+ADONAY SOUZA FERREIRA
+ALINE DAFFINY FERREIRA GOMES
+LETÍCIA MINELVINO DA COSTA
+
+Exercício 001: 
+Crie um programa que escreva "Olá, mundo!" na tela. """
 
 msg = "Olá, Mundo!"
 print(msg)
