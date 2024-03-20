69760ce669eceb943074efc10fbe730e29600746
Letícia Minelvino <leticiaminelvino@gmail.com>
2024-03-20 11:09:09 -0300

    Update ex36.py

--- 36 - 70/ex36.py
+++ 36 - 70/ex36.py
@@ -7,7 +7,8 @@ Leticia Minelvino da Costa
 Exercício Python 036:
 Construa um  programa para aprovar o empréstimo bancário para a compra de uma casa. Pergunte o valor da casa,
 salário do comprador e em quantos anos ele deseja quitar a dívida. A prestação não pode exceder 30% do salário ou
-então será negado o empréstimo """
+então será negado o empréstimo 
+"""
 
 print('bem vindo ao seu banco de empréstimos')
 valor_da_casa = float(input('por favor, informe o valor da casa que você quer comprar: '))
@@ -20,9 +21,3 @@ if num_minimo_prestacoes > ((salario_da_pessoa * 0.3)):
     print(f'empréstimo negado, as prestações passam 30% do seu salário')
 else:
     print(f'empréstimo aceito, você deve pagar R$ {num_minimo_prestacoes:.2f} mensalmente por {anos_a_pagar_casa * 12} meses')
-
-
-
-
-
-
