b7a866e00457227f3cca659cf12562a674c47e5d
Adonaay <161964280+Adonaay@users.noreply.github.com>
2024-03-22 01:40:11 -0400

    Create Ex46.py

---
+++ 36 - 70/Ex46.py
@@ -0,0 +1,16 @@
+ADONAY SOUZA FERREIRA
+ALINE DAFFINY FERREIRA GOMES
+LETÍCIA MINELVINO DA COSTA
+
+Exercicio 046:
+Crie um programa que mostre na tela uma contagem regressiva para estouro de fogos de
+de artifício, indo de 10 até 0, com pausa de 1 segundo entre elas.
+'''
+from time import sleep
+for contagem in range (10,-1,-1):
+    print(contagem)
+    sleep(1)
+print('''BUM!
+  BUM!
+     POW!
+        FELIZ ANO NOVO !''')
