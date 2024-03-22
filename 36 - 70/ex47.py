56816fe4e4d5415a8c9f5fdb2dc25db1e14eb7f3
Adonaay <161964280+Adonaay@users.noreply.github.com>
2024-03-22 01:41:37 -0400

    ex47.py

---
+++ 36 - 70/ex47.py
@@ -0,0 +1,12 @@
+'''
+Alunos:
+ADONAY SOUZA FERREIRA
+ALINE DAFFINY FERREIRA GOMES
+LETÍCIA MINELVINO DA COSTA
+
+Exercicio 047:
+Crie um programa que mostre na tela  todos os números pares  que estão no intervalo de 1 a 50.
+'''
+for par in range(2,51,2):
+    print(par, end=' ')
+print('FIM!')
